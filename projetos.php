8a2b8df0d66668d21539925e92041c390d6718a9
Rodolfo Lara Cassaro <97470726+rodolfolarac@users.noreply.github.com>
2025-03-25 08:19:18 -0300

    feat: Finalized codebase

---
+++ projetos.php
@@ -0,0 +1,24 @@
+<?php
+$projetos = [
+    [
+        "titulo" => "Meu Portfolio",
+        "finalizado" => true,
+        "data" => "2025-03-19",
+        "descricao" => "Meu portfolio criado em HTML e PHP.",
+        "stack" => ["PHP", "HTML", "CSS", "JS"]
+    ],
+    [
+        "titulo" => "Lista de Tarefas",
+        "finalizado" => false,
+        "data" => "2025-03-19",
+        "descricao" => "Meu portfolio criado em HTML e PHP.",
+        "stack" => ["React", "HTML", "JS"]
+    ],
+    [
+        "titulo" => "Controle de Leitura de Livros",
+        "finalizado" => false,
+        "data" => "2025-03-19",
+        "descricao" => "Meu portfolio criado em HTML e PHP.",
+        "stack" => ["PHP", "HTML", "Tailwind"]
+    ],
+];
